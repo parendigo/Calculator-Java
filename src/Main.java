73c24f284171927b1b40622bceb38e548b57a191
Artem <pam.artem@mail.ru>
2020-11-23 23:11:48 +0300

    rename main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class Main {
     public static void main(String[] args) {
         int i = 0;
         System.out.println("*Simple Calculator*\nFor more info: [!Info]");
